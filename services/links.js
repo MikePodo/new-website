0c5aee2ae632cb7662f1594392d4e8a30d481ee1
Michael Podolsky <michaelspodolsky@gmail.com>
2022-05-02 00:23:08 -0700

    clean up links module export

--- services/links.js
+++ services/links.js
@@ -1,8 +1,6 @@
-const links = {
+module.exports = {
   GITHUB: "https://github.com/MikePodo",
   LINKEDIN: "https://www.linkedin.com/in/mikepodo/",
   RESUME:
     "https://drive.google.com/file/d/1tzMfjwZK4PVkqqliT5-pxrgU2qJs-EYE/view?usp=sharing",
 };
-
-module.exports = links;
