d405e992da60b298bf7d0faaaec4aaf3fb433583
MikePodo <michaelspodolsky@gmail.com>
2022-10-30 00:45:48 -0700

    remove vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import { Analytics } from "@vercel/analytics/react";
+// import { Analytics } from "@vercel/analytics/react";
 import { library, config } from "@fortawesome/fontawesome-svg-core";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
@@ -21,7 +21,7 @@ function MyApp({ Component, pageProps }) {
         <Vanta />
         <Navbar />
         <Component {...pageProps} />
-        <Analytics />
+        {/* <Analytics /> */}
       </ThemeProvider>
     </div>
   );
